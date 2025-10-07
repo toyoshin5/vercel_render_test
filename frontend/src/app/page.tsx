51ed2ab82c715007ce037f33a68c06ad6e83544b
豊田 真吾 <toyodasinngo@gmail.com>
2025-10-08 00:19:07 +0900

    ワークフロー削除

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   const [newMemo, setNewMemo] = useState("");
 
   useEffect(() => {
-    fetchMem
+    fetchMemos();
   }, []);
 
   const fetchMemos = async () => {
